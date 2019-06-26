de0f390a4ffe159da913ee6473920a4c5c6304c8
Caio Cozza <caiocozza.art@gmail.com>
2019-06-26 20:53:24 -0300

    challange 2

--- challange_one.cpp
+++ challange_one.cpp
@@ -5,8 +5,10 @@
 
 //This is a series of challanges that I'll be solving using STL
 
-//1 - Given an int container, count the repeating numbers once.
-//example 1 2 1 3 4 1 2 > results in 2. The 1 and 2 appears more than once
+//[std::sort, std::adjacent_difference, std::count_if]
+//(1) Given an int container, create a sum of the elements that appears
+//more than once.
+//Example: 1 2 1 3 2 > results in 2. The 1 and 2 appears more than once
 auto challange1(std::vector<int> const& c) {
     std::vector<int> temp(c); //makes a copy
     std::sort(std::begin(temp), std::end(temp)); //sorts the temp container
@@ -23,4 +25,24 @@ auto challange1(std::vector<int> const& c) {
     return std::count_if(std::next(std::cbegin(temp)),
                          std::cend(temp),
                          [](int a) { return a == 0;});
+}
+
+//(2) Given an int container, count its elements that repeats sequentially.
+//Example: 1 2 2 3 4 4 5 > results in 2.
+auto challange2(std::vector<int> const& c) {
+    //with 2 containers, check from the first to the previous of the last
+    //put the second one starting from the beggining plus one until the end
+    //since the elements are repeated sequentially, we check if they are equal
+    //if true, sum.
+    //consider the vector  1 2 2 3 4 4 5
+    //                   > 1 2 2 3 4 4
+    //                   > 2 2 3 4 4 5
+    //                   > 0 1 0 0 1 0 = 2
+    //two elements repeats sequentially
+    return std::inner_product(std::cbegin(c),
+                              std::prev(std::cend(c)),
+                              std::next(std::cbegin(c)),
+                              0,
+                              std::plus{},
+                              std::equal_to{});
 }
